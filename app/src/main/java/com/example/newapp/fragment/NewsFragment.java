9a16d3b48dd2a9146bbcd844a7ac86a071136e8e
Leon <861265601@qq.com>
2022-03-15 15:45:55 +0800

    Upload News fragment

--- app/src/main/java/com/example/newapp/fragment/NewsFragment.java
+++ app/src/main/java/com/example/newapp/fragment/NewsFragment.java
@@ -1,43 +1,62 @@
 package com.example.newapp.fragment;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.newapp.R;
+import com.example.newapp.adapter.NewsAdapter;
+import com.example.newapp.api.Api;
+import com.example.newapp.api.ApiConfig;
+import com.example.newapp.api.TtitCallBack;
+import com.example.newapp.entity.NewsEntity;
+import com.example.newapp.entity.NewsListResponse;
+import com.google.gson.Gson;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.listener.OnLoadMoreListener;
+import com.scwang.smartrefresh.layout.listener.OnRefreshListener;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link NewsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class NewsFragment extends Fragment {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
-//    // TODO: Rename parameter arguments, choose names that match
-//    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-//    private static final String ARG_PARAM1 = "param1";
-//    private static final String ARG_PARAM2 = "param2";
-//
-//    // TODO: Rename and change types of parameters
-//    private String mParam1;
-//    private String mParam2;
+
+public class NewsFragment extends BaseFragment {
+
+
+    private RecyclerView recyclerView;
+    private RefreshLayout refreshLayout;
+    private NewsAdapter newsAdapter;
+    private List<NewsEntity> datas = new ArrayList<NewsEntity>();
+    private LinearLayoutManager linearLayoutManager;
+    private int pageNum = 1;
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case 0:
+                    newsAdapter.setDatas(datas);
+                    newsAdapter.notifyDataSetChanged();
+                    break;
+            }
+        }
+    };
+
 
     public NewsFragment() {
-        // Required empty public constructor
-    }
 
-    //    /**
-//     * Use this factory method to create a new instance of
-//     * this fragment using the provided parameters.
-//     *
-//     * @param param1 Parameter 1.
-//     * @param param2 Parameter 2.
-//     * @return A new instance of fragment NewsFragment.
-//     */
-//    // TODO: Rename and change types and number of parameters
+    }
     public static NewsFragment newInstance() {
         NewsFragment fragment = new NewsFragment();
 
@@ -45,15 +64,98 @@ public class NewsFragment extends Fragment {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
+    protected int initLayout() {
+        return R.layout.fragment_news;
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_news, container, false);
+    protected void initView() {
+        recyclerView = mRootView.findViewById(R.id.recyclerView);
+        refreshLayout = mRootView.findViewById(R.id.refreshLayout);
     }
+
+    @Override
+    protected void initData() {
+        linearLayoutManager = new LinearLayoutManager(getActivity());
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        newsAdapter = new NewsAdapter(getActivity());
+        recyclerView.setAdapter(newsAdapter);
+        newsAdapter.setOnItemClickListener(new NewsAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(Serializable obj) {
+                NewsEntity newsEntity = (NewsEntity) obj;
+                String url = "http://192.168.31.32:8089/newsDetail?title=" + newsEntity.getAuthorName();
+                Bundle bundle = new Bundle();
+                bundle.putString("url", url);
+               // navigateToWithBundle(WebActivity.class, bundle);
+
+                showToast("ddd");
+            }
+
+
+        });
+        refreshLayout.setOnRefreshListener(new OnRefreshListener() {
+            @Override
+            public void onRefresh(RefreshLayout refreshlayout) {
+                pageNum = 1;
+                getNewsList(true);
+            }
+        });
+        refreshLayout.setOnLoadMoreListener(new OnLoadMoreListener() {
+            @Override
+            public void onLoadMore(RefreshLayout refreshlayout) {
+                pageNum++;
+                getNewsList(false);
+            }
+        });
+        getNewsList(true);
+    }
+    private void getNewsList(final boolean isRefresh) {
+            HashMap<String, Object> params = new HashMap<>();
+            params.put("page", pageNum);
+            params.put("limit", ApiConfig.PAGE_SIZE);
+            Api.config(ApiConfig.NEWS_LIST, params).GetRequest(getActivity(), new TtitCallBack() {
+
+                @Override
+                public void OnSuccess(String res) {
+                    if (isRefresh) {
+                        refreshLayout.finishRefresh(true);
+                    } else {
+                        refreshLayout.finishLoadMore(true);
+                    }
+                    NewsListResponse response = new Gson().fromJson(res, NewsListResponse.class);
+                    if (response != null && response.getCode() == 0) {
+                        List<NewsEntity> list = response.getPage().getList();
+                        if (list != null && list.size() > 0) {
+                            if (isRefresh) {
+                                datas = list;
+                            } else {
+                                datas.addAll(list);
+                            }
+                            mHandler.sendEmptyMessage(0);
+                        } else {
+                            if (isRefresh) {
+                                showToastSync("暂时无数据");
+                            } else {
+                                showToastSync("没有更多数据");
+                            }
+                        }
+                    }
+                }
+
+                @Override
+                public void onFailure(Exception exception) {
+                    if (isRefresh) {
+                        refreshLayout.finishRefresh(true);
+                    } else {
+                        refreshLayout.finishLoadMore(true);
+                    }
+                }
+            });
+        }
+
+
+
 }
+
